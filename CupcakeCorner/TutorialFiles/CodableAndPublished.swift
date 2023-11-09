a9e6c9be32ad6ec11f7fcda71fc3fded8352ca53
Scott Crowder <scrowder@gmail.com>
2023-11-09 17:30:36 -0600

    Attempting to follow course but XCode 15 has changed some options

--- CupcakeCorner/TutorialFiles/CodableAndPublished.swift
+++ CupcakeCorner/TutorialFiles/CodableAndPublished.swift
@@ -7,12 +7,43 @@
 
 import SwiftUI
 
+class User: ObservedObject, Codable {
+    var name: String = "Paul Hudson"
+}
+
 struct CodableAndPublished: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+
+@Observable
+class Model {
+    var myString: String = "Hello"
+}
+
+struct MediumArticle: View {
+    @State var model: Model
+    @State private var changeText: Bool = true
+
+        var body: some View {
+            VStack {
+                Text("myString: \(model.myString)")
+                Button("Hit me") {
+                    changeText.toggle()
+                    if changeText {
+                        model.myString = "new"
+                    } else {
+                        model.myString = "old"
+                    }
+                }
+            }
+        }
+}
+
 #Preview {
-    CodableAndPublished()
+    let model: Model = Model()
+    //CodableAndPublished()
+    return MediumArticle(model: model)
 }
