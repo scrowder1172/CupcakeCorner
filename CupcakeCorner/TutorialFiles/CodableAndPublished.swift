547613f51d321cb56cc74b6a58854011b6de902a
Scott Crowder <scrowder@gmail.com>
2023-11-09 12:20:14 -0600

    add file

---
+++ CupcakeCorner/TutorialFiles/CodableAndPublished.swift
@@ -0,0 +1,18 @@
+//
+//  CodableAndPublished.swift
+//  CupcakeCorner
+//
+//  Created by SCOTT CROWDER on 11/9/23.
+//
+
+import SwiftUI
+
+struct CodableAndPublished: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodableAndPublished()
+}
