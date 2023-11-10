bae4d2a0973bfa675de0b55e77d35418215594d8
Scott Crowder <scrowder@gmail.com>
2023-11-10 12:31:54 -0600

    grab list of Taylor Swift songs from iTunes

--- CupcakeCorner/TutorialFiles/CodableAndURLSession.swift
+++ CupcakeCorner/TutorialFiles/CodableAndURLSession.swift
@@ -21,13 +21,40 @@ struct CodableAndURLSession: View {
     @State private var results: [Result] = [Result]()
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        NavigationStack {
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.collectionName)
+                }
             }
+            .task {
+                await loadData()
+            }
+            .navigationTitle("Taylor Swift")
         }
+    }
+    
+    func loadData() async {
+        // create URL to read from
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        // fetch data from URL
+        do {
+            let(data, _) = try await URLSession.shared.data(from: url)
+            
+            // decode data and display
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
     }
 }
 
