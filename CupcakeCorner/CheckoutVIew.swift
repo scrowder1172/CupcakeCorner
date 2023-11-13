206bf0c324577f728498fe91ffb9d32419163f20
Scott Crowder <scrowder@gmail.com>
2023-11-13 16:47:39 -0600

    create initial view

--- CupcakeCorner/CheckoutVIew.swift
+++ CupcakeCorner/CheckoutVIew.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct CheckoutVIew: View {
+    var order: Order
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Let's checkout...")
     }
 }
 
 #Preview {
-    CheckoutVIew()
+    CheckoutVIew(order: Order())
 }
