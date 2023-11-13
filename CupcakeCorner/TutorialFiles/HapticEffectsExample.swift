bd85be08e0e234c61deb410277e895a05bec00e3
Scott Crowder <scrowder@gmail.com>
2023-11-13 13:21:12 -0600

    add file

---
+++ CupcakeCorner/TutorialFiles/HapticEffectsExample.swift
@@ -0,0 +1,18 @@
+//
+//  HapticEffectsExample.swift
+//  CupcakeCorner
+//
+//  Created by SCOTT CROWDER on 11/13/23.
+//
+
+import SwiftUI
+
+struct HapticEffectsExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HapticEffectsExample()
+}
